51d10d6fbf3a277a1bac18d4fcb66aa9dd59ec4e
stoyan <stoyan.zhekov@abv.bg>
2015-03-10 22:04:19 +0200

    Revert "emo's task"
    This reverts commit f7394d9f88023317f46cbd9c81b8c85ee77a9085.

--- src/main/java/bg/hotel/controller/ReceptionController.java
+++ src/main/java/bg/hotel/controller/ReceptionController.java
@@ -1,14 +1,20 @@
 package bg.hotel.controller;
 
+import bg.hotel.entities.ReservationDetails;
 import bg.hotel.entities.Room;
 import bg.hotel.services.ReceptionService;
 import org.springframework.context.annotation.Scope;
 
-import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Scope("session")
@@ -20,161 +26,84 @@ public class ReceptionController {
 
 	private PeriodDto period = new PeriodDto();
     private List<Room> rooms = new ArrayList<>();
-    private List<DynamicRoom> dynamicRooms = new ArrayList<>();
-    private List<ColumnModel> columns;
-
-    private String columnTemplate = "number day1 day2 day3 day4 day5 day6 day7";
-
-    @PostConstruct
-    public void init() {
-        initRooms();
-
-        createDynamicColumns();
-    }
-
-	public PeriodDto getPeriod() {
-		return period;
-	}
-
-	public void setPeriod(PeriodDto period) {
-		this.period = period;
-	}
+    private List<String> days = new ArrayList<>();
+    private DateFormat df = new SimpleDateFormat("d/M/y");
+    private final long DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
 
     public void initRooms() {
-        rooms = receptionService.fetchAllRooms();
-        for (Room r :rooms) {
-            DynamicRoom dr = new DynamicRoom();
-            dr.number = r.getNumber();
-            dr.day1 = "v";
-            dr.day2 = "x";
-            dr.day3 = "x";
-            dr.day4 = "x";
-            dr.day5 = "v";
-            dr.day6 = "v";
-            dr.day7 = "v";
-            dynamicRooms.add(dr);
+        if (rooms.isEmpty()) {
+            rooms = receptionService.fetchAllRooms();
+        }
+    }
+
+    public void calculateDays() {
+        validateInput();
+        clearDays();
+        int diffInDays = (int) ((period.getTo().getTime() - period.getFrom().getTime())/ DAY_IN_MILLIS );
+        Date dateToAdd = period.getFrom();
+        addDateToArray(dateToAdd);
+        for (int i = 1; i <= diffInDays; i++) {
+            dateToAdd = addOneDay(dateToAdd);
+            addDateToArray(dateToAdd);
+        }
+    }
+
+    private void validateInput() {
+        if (period.getFrom().after(period.getTo())) {
+            FacesContext.getCurrentInstance().addMessage("newPassword", new FacesMessage("Error: Date 'From' is after Date 'To'. Please correct!"));
+            return;
+        }
+    }
+
+    private void addDateToArray(Date dateToAdd) {
+        String date = df.format(dateToAdd);
+        days.add(date);
+    }
+
+    private void clearDays() {
+        days = new ArrayList<>();
+    }
+
+    public Date addOneDay(Date date) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.DATE, 1);
+        date = c.getTime();
+        return date;
+    }
+
+    public boolean isFree(Room room, String day) {
+        Date date = getDateFromString(day);
+        for (ReservationDetails rd : room.getReservationDetail()) {
+             if (rd.getCheckIn().getTime() <= date.getTime() &&
+                     rd.getCheckOut().getTime() > date.getTime()) {
+                 return false;
+             }
+        }
+        return true;
+    }
+
+    private Date getDateFromString(String day) {
+        DateFormat format = new SimpleDateFormat("d/M/y");
+        Date date = null;
+        try {
+            date = format.parse(day);
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
+        return date;
+    }
+
+    public PeriodDto getPeriod() {
+        return period;
+    }
+
+    public List<String> getDays() {
+        return days;
     }
 
     public List<Room> getRooms() {
         return rooms;
-    }
-
-    public void setRooms(List<Room> rooms) {
-        this.rooms = rooms;
-    }
-
-    private void createDynamicColumns() {
-        String[] columnKeys = columnTemplate.split(" ");
-        columns = new ArrayList<ColumnModel>();
-
-        for (String columnKey : columnKeys) {
-            String key = columnKey.trim();
-
-            columns.add(new ColumnModel(columnKey.toUpperCase(), columnKey));
-        }
-    }
-
-    public List<ColumnModel> getColumns() {
-        return columns;
-    }
-
-    static public class ColumnModel implements Serializable {
-
-        private String header;
-        private String property;
-
-        public ColumnModel(String header, String property) {
-            this.header = header;
-            this.property = property;
-        }
-
-        public String getHeader() {
-            return header;
-        }
-
-        public String getProperty() {
-            return property;
-        }
-    }
-
-    static public class DynamicRoom implements Serializable {
-        private int number;
-        private String day1;
-        private String day2;
-        private String day3;
-        private String day4;
-        private String day5;
-        private String day6;
-        private String day7;
-
-        public int getNumber() {
-            return number;
-        }
-
-        public String getDay1() {
-            return day1;
-        }
-
-        public void setDay1(String day1) {
-            this.day1 = day1;
-        }
-
-        public String getDay2() {
-            return day2;
-        }
-
-        public void setDay2(String day2) {
-            this.day2 = day2;
-        }
-
-        public String getDay3() {
-            return day3;
-        }
-
-        public void setDay3(String day3) {
-            this.day3 = day3;
-        }
-
-        public String getDay4() {
-            return day4;
-        }
-
-        public void setDay4(String day4) {
-            this.day4 = day4;
-        }
-
-        public String getDay5() {
-            return day5;
-        }
-
-        public void setDay5(String day5) {
-            this.day5 = day5;
-        }
-
-        public String getDay6() {
-            return day6;
-        }
-
-        public void setDay6(String day6) {
-            this.day6 = day6;
-        }
-
-        public String getDay7() {
-            return day7;
-        }
-
-        public void setDay7(String day7) {
-            this.day7 = day7;
-        }
-    }
-
-    public List<DynamicRoom> getDynamicRooms() {
-        return dynamicRooms;
-    }
-
-    public void setDynamicRooms(List<DynamicRoom> dynamicRooms) {
-        this.dynamicRooms = dynamicRooms;
     }
 }
